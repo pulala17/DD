1b7d9b43dc3ca3155c7176b071631fe5519c77b6
Xaria <89126668+pulala17@users.noreply.github.com>
2023-05-09 15:34:29 +0800

    Delete 2_patient.py

--- 2_patient.py
+++
@@ -1,9 +0,0 @@
-full_name = 'John Smith'
-age = 20
-is_new = True 
-
-name = input('What is your name? ')
-print('Hi ' + name)
-
-color = input('What is your favorite color? ')
-print(name + ' likes ' + color)
